3ff18cf075eb266dce3880cb3da6400fd2da8334
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-14 17:35:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import openai
 import streamlit as st
-from pydantic import BaseModel
-
-class Query(BaseModel):
-    question: str
 
 # Ajouter votre clé API OpenAI
 openai.api_key = "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"
@@ -13,20 +9,31 @@ model_engine = "text-davinci-002"
 temperature = 0.5
 max_tokens = 100
 
+# Définir la base de connaissances
+knowledge_base = """
+Q: Quelle est la capitale de la France ?
+A: La capitale de la France est Paris.
+
+Q: Quand a été fondée l'OpenAI ?
+A: L'OpenAI a été fondée en 2015.
+
+Q: Qui est l'inventeur de la théorie de la relativité ?
+A: L'inventeur de la théorie de la relativité est Albert Einstein.
+"""
+
 # Créer l'interface utilisateur avec Streamlit
 def main():
     st.title("Bot GPT-3")
 
-    query = Query(question="")
-    query.question = st.text_input("Posez votre question :")
+    question = st.text_input("Posez votre question :")
 
     if st.button("Répondre"):
-        response = generate_response(query.question)
+        response = generate_response(question)
         st.write(response)
 
 # Définir une fonction pour générer une réponse à partir de GPT-3
 def generate_response(question):
-    prompt = f"Q: {question}\nA:"
+    prompt = f"{knowledge_base}\nQ: {question}\nA:"
     response = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
